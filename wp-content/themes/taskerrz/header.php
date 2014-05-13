9c6a736fbad796ea925a51da71753303ab81abce
souvikchaudhury <souvik.chaudhury@gmail.com>
2014-05-13 13:20:18 +0530

    ght

--- wp-content/themes/taskerrz/header.php
+++ wp-content/themes/taskerrz/header.php
@@ -39,7 +39,6 @@
 <script type="text/javascript" src="<?php echo get_template_directory_uri().'/js/jquery.elastislide.js'; ?>"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
-		// hello test
 		$('.getstartedsubmit').click(function(){
 			usr = jQuery('.chkvalidateusr').val();
 			eml = jQuery('.chkvalidateemail').val();
